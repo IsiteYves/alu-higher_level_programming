d0851d533a0926624929952bf2b5fdcb08fb2425
IsiteYves <yvesisite@gmail.com>
2026-03-20 13:13:09 -0700

    test

--- python-hello_world/102-main.py
+++ python-hello_world/102-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from 102-magic_calculation import magic_calculation
+# We use __import__ because the filename starts with a number
+magic_calculation = __import__("102-magic_calculation").magic_calculation
 
 a = 2
 b = 3
