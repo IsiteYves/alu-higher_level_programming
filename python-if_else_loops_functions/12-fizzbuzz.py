b4af9b3be7fcdbc5d1b372558900af9e99fbc12c
IsiteYves <yvesisite@gmail.com>
2026-03-20 12:32:52 -0700

    fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,7 +8,5 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz", end="")
         else:
-            print(f"{i}", end="")
-        
-        if i < 100:
-            print(" ", end="")
+            print("{}".format(i), end="")
+        print(" ", end="")
