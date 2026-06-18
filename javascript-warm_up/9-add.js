a04ae2612aa043a39fa852571aa43bef7b879d59
IsiteYves <yvesisite@gmail.com>
2026-06-18 05:24:02 -0700

    amend

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+const num1 = Math.floor(Number(process.argv[2]));
+const num2 = Math.floor(Number(process.argv[3]));
+console.log(add(num1, num2));
